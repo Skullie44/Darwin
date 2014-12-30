15cd3b2b6490d1121e2745eb6a3dd4eeba443de8
Skullie44 <skullie44@gmail.com>
2014-12-30 15:47:26 -0500

    Update Create.py
    cleaning up code and structure

--- Create.py
+++ Create.py
@@ -1,20 +1,14 @@
-__author__ = 'skullie'
+__author__ = 'ADM_Skullie'
 
-import Creature.py
+import Animal.py
 import Validate.py
 
 class Create:
 
-    terrain = Validate.ver_terrain(input('where will this creature live?'))
-    locomotion = Validate.ver_locomotion(input('how will this creature move?'))
-    blood = Validate.ver_blood(input('is it warm blooded? Y/N'))
-    respiratory = Validate.ver_respiratory(input('how does this creature breath?'))
-    birth = Validate.ver_birth(input('does this creature lay eggs? Y/N'))
-    offspring = Validate.ver_offspring(input('how many offspring does it typically have?'))
-    care = Validate.ver_care(input('do they care for their young? Y/N'))
-    skin = Validate.ver_skin(input('what type of skin does it have?'))
-    nest = input('does this animal have a nest? Y/N')
-
+    locomotion = Validate.ver_locomotion(input('How does this creature move?'))
+    blood = Validate.ver_blood(input('Does this creature have warm blood? Y/N'))
+    respiratory = Validate.ver_respiratory(input('How does this creature breath?'))
+    birth = Validate.ver_birth(input('How does this creature lay eggs? Y/N'))
 
 
 
@@ -22,13 +16,9 @@ class Create:
     animal['blood'] = blood
     animal['respiratory'] = respiratory
     animal['birth']= birth
-    animal['offspring'] = offspring
-    animal['care'] = care
     animal['locomotion'] = locomotion
-    animal['terrain'] = terrain
 
 
-
-    creature = Creature(animal)
+    creature = Animal(animal)
 
     print "you've created a " + creature.name()
