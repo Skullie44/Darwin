fba02a653f9607de80c32e38f52fc46e1a6e0c35
Skullie44 <skullie44@gmail.com>
2015-11-14 11:47:14 -0500

    updating architecture change to application

--- Creature.py
+++ Creature.py
@@ -6,9 +6,3 @@ class Creature:
     def __init__(self):
         pass
 
-
-    if blood = 'cold'
-        if terrain = 'water'
-            new Amphibian(blood, respiratory, birth, offspring, care, locomotion, terrain, skin, metamorphasis)
-        elif terrain = 'land'
-            new Reptile(blood, respiratory, birth, offspring, care, locomotion, terrain, skin, nest)
